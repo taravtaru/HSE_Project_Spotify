1fc37bb833c235455b97ab7fb9648fd1c7d7556b
taravtaru <taravtaru@gmail.com>
2023-10-07 13:32:06 +0300

    Most popular artist and year stats functions added

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import pandas as pd
 import argparse
-from typing import List
-
+from typing import List, Dict
 
 def open_file(path: str) -> List[List[object]]:
     df = pd.read_csv(path)
 
     return df.values.tolist()
+
+def most_popular_artist(table: List[List[object]]):
+    songs_count = {}
+    for i in table:
+        if i[7] in songs_count:
+            songs_count[i[7]] += 1
+        else:
+            songs_count[i[7]] = 1
+    return max(songs_count, key=songs_count.get)
+
+def get_year_stats(table: List[List[object]]) -> Dict:
+    years_count = {}
+    for i in table:
+        if i[1] in years_count:
+            years_count[i[1]] += 1
+        else:
+            years_count[i[1]] = 1
+    return years_count
 
 
 if __name__ == "__main__":
@@ -18,4 +35,4 @@ if __name__ == "__main__":
 
     table = open_file(args.file_path)
 
-    print(len(table))
+    print(most_popular_artist(table))
